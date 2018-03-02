d5b0a7cd45ebb3a938001f9f6b0b8d3d7fac991b
yongyaunzhiqizhi <1219674323@qq.com>
2018-03-02 11:49:05 +0800

    首页&项目列表的第三次添加

---
+++ Src/GoodGayFriend/GGF/Theme/Js/ProjectList.js
@@ -0,0 +1,15 @@
+﻿
+
+
+var Portfolio = function () {
+    return {
+        init: function () {
+            $('.mix-grid').mixitup();
+        }
+    };
+}()
+
+$(function () {
+    Portfolio.init();
+});
+
